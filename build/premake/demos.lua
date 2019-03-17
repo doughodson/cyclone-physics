332d066bf3e9d4be37e6208a8c0ba1ebf4ab1dec
Doug Hodson <doug@sidechannel.net>
2019-03-16 23:57:59 -0400

    added demo

--- build/premake/demos.lua
+++ build/premake/demos.lua
@@ -22,3 +22,25 @@ project "demo-ballistic"
       links { "lib-cyclone", "lib-freeglut2" }
    end
 
+project "demo-bigballistic"
+   targetname  "bigballistic"    -- target filename
+   targetdir   "../../bin"       -- destination dir for compiled binary target
+   debugdir    "../../bin"       -- destination dir for debug info
+   symbolspath "$(OutDir)$(TargetName).pdb"
+   kind        "ConsoleApp"      -- creating a console app
+   includedirs {
+      CYCLONE_IncPath,
+      "../../deps/src/freeglut-2.8.1/include",
+   }
+   libdirs     { "../../lib", "../../deps/lib" }
+   defines     { "FREEGLUT_STATIC", "FREEGLUT_LIB_PRAGMAS=0" }
+   files {
+      "../../src/demos/*.*",
+      "../../src/demos/bigballistic/*.*",
+   }
+   if os.ishost("windows") then
+      links { "lib-cyclone", "lib-freeglut2", "glu32", "opengl32", "winmm" }
+   end
+   if os.ishost("linux") then
+      links { "lib-cyclone", "lib-freeglut2" }
+   end
